f70930d5eea70e28648325ebf9d162c769bdd0c6
Alan <alan.cd@qq.com>
2018-06-16 19:27:03 +0800

    提交产品相关

--- core/myorm_core.php
+++ core/myorm_core.php
@@ -106,21 +106,37 @@ class myorm_core{
         return [$offset, $pageSize, $page, $data];
     }
 
-    protected function fastQuery($sql, $params = [])
+    protected function fulltextSearch(array $fields, string $keywords, string $paramName = 'keywords')
+    {
+        if (FULLTEXT_SUPPORT) {
+            $match = implode(',', $fields);
+            $filter = 'MATCH ($match) AGAINST (:{$paramName} IN BOOLEAN MODE)';
+            $param = $keywords;
+        } else {
+            foreach ($fields as $field) {
+                $filter[] = '{$field} like :{$paramName}';
+            }
+            $filter = "(" . implode(' or ', $filter) . ")";
+            $param = '%' . $keywords . '%';
+        }
+        return [$filter, $paramName, $param];
+    }
+
+    protected function fastQuery(string $sql, array $params = [])
     {
         $pdo = new \core\lib\model;
         if(empty($params)) {
             return $pdo->query($sql);
         }
         $stmt = $pdo->prepare($sql);
-        foreach($param as $n => $value) {
-            $pdo->bindValue($n, $value);
+        foreach($params as $n => $value) {
+            $stmt->bindValue($n, $value);
         }
         return $stmt->execute();
     }
 
 
-    protected function dataForCreate($data, $allowFields=[], $fixed=[])
+    protected function dataForCreate(array $data, array $allowFields=[], array $fixed=[])
     {
         if ($allowFields) {
             $data = array_intersect_key($data, array_fill_keys($allowFields, 1));
@@ -139,7 +155,7 @@ class myorm_core{
         return [implode(',', $fields), implode(',', $values), $data];
     }
 
-    protected function dataForUpdate($data, $allowFields=[])
+    protected function dataForUpdate(array $data, array $allowFields=[])
     {
         if ($allowFields) {
             $data = array_intersect_key($data, array_fill_keys($allowFields, 1));
@@ -153,7 +169,7 @@ class myorm_core{
         return [implode(', ', $fields), $data];
     }
 
-    protected function fastInsert($sql, $data)
+    protected function fastInsert(string $sql, array $data)
     {
         $pdo = new \core\lib\model;
         $stmt = $pdo->prepare($sql);
@@ -165,8 +181,56 @@ class myorm_core{
         return [$effected, $lastId];
     }
 
+    public function bulkInsert(string $tableName, array $rows, $ignoreDuplicate = false)
+    {
+        if (!is_array($rows) || !is_array(current($rows))) {
+            throw new Exception("param rows must be a 2D array");
+        }
 
-    protected function fastUpdate($sql, $data, $param = [])
+        $row = current($rows);
+        if (empty($row)) {
+            throw new Exception("param rows must be a 2D array");
+        }
+
+        $fields = array_keys($row);
+        $binding = [];
+        foreach($fields as $field) {
+            $binding[] = ":" . $field;
+        }
+
+        // MSSQL: 利用索引选项 CREATE UNIQUE INDEX AK_Index ON #Test (C1) WITH (IGNORE_DUP_KEY = ON);
+        // postgres 9.5:  insert into citys values('Hanzhoug'),('shenzhen') on conflict do nothing
+        $ignore = $ignoreDuplicate ? 'IGNORE' : '';
+
+        $sql = "INSERT $ignore INTO /*prefix*/$tableName ("
+         . implode(", ", $fields) . ") VALUES ("
+         . implode(', ', $binding)
+         . ")";
+         
+        $pdo = new \core\lib\model;
+        $stmt = $pdo->prepare($sql);
+        foreach ($rows as $row) {
+            foreach ($fields as $field) {
+                $stmt->bindValue(":{$field}", $row[$field] ?? null);
+            }
+            $effected += $stmt->execute();
+        }
+        $stmt   = null;
+
+        return $effected;
+    }
+
+    // 更新关联子表的数据
+    public function columnOf($table, $columnName)
+    {
+        $data = [];
+        foreach($table as $row) {
+            $data[] = $row[$columnName];
+        }
+        return $data;
+    }
+
+    protected function fastUpdate(string $sql, array $data, array $param = [])
     {
         $pdo = new \core\lib\model;
         $stmt = $pdo->prepare($sql);
@@ -178,6 +242,46 @@ class myorm_core{
         }
         $effected = $stmt->execute();
         return $effected;
+    }
+
+    protected function combineFields(array $table, string $field, array $subTable, string $field2)
+    {
+        $keyMap = [];
+        foreach($subTable as $row) {
+            $key = $row[$field2];
+            unset($row[$field2]);
+            $keyMap[$key] = $row;
+        }
+
+        foreach($table as $idx => $row) {
+            $key = $row[$field];
+            if (isset($keyMap[$key])) {
+                $keyMap[$key] =  array_merge($keyMap[$key], $row);
+            } else {
+            }
+        }
+        return array_values($keyMap);
+    }
+
+    protected function combineList(array $table, string $field, array $subTable, string $field2, $asName = 'xx')
+    {
+        $keyMap = [];
+        foreach($subTable as $row) {
+            $key = $row[$field2];
+            unset($row[$field2]);
+            if (isset($keyMap[$key])) {
+                $keyMap[$key][] = $row;
+            } else {
+                $keyMap[$key] = [$row];
+            }
+        }
+
+        foreach($table as $key => $row) {
+            if (isset($keyMap[$key])) {
+                $table[$key][$asName] = $keyMap[$key];
+            }
+        }
+        return array_values($table);
     }
 
 }
