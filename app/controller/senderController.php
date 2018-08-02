dc28f412047d02b0f228471912363ddf4e4477f9
dupeng <412499907@qq.com>
2018-08-02 22:58:11 +0800

    fix sender

--- app/controller/senderController.php
+++ app/controller/senderController.php
@@ -107,7 +107,7 @@ class senderController extends \core\myorm_core{
         // 固定值, 补充或覆盖到 $data 中
         $openid = $_SESSION['openid'];
         $fixed = [
-            '_openid' => $openid,
+            'openid' => $openid,
             'status' => 2,
         ];
 
